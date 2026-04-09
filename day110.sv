0355381476148e77b45bcc86a36c334d5471fd5a
charanmanapati <manapati2003@gmail.com>
2026-04-09 16:21:18 +0530

    day110

---
+++ day110.sv
@@ -0,0 +1,21 @@
+this is head 
+,brmeamband iam write this
+sdveb;e.bt
+rherb,[,e,b
+for
+       	checking
+       	for head
+       
+
+	and tail
+which \
+e,g;e;.rg;
+mernbnhbe
+r
+v
+e'b
+'e
+tb'
+r't
+'
+rtrt;.
